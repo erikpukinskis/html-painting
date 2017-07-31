0c3f64883a234c7cbd541b50ce5581fe293bd9a3
Erik Pukinskis <erik@snowedin.net>
2017-07-30 17:54:26 -0700

    play paintings back

--- html-painting.js
+++ html-painting.js
@@ -2,14 +2,15 @@ var library = require("module-library")(require)
 
 module.exports = library.export(
   "html-painting",
-  ["identifiable"],
-  function(identifiable) {
+  ["identifiable", "add-html", "web-element"],
+  function(identifiable, addHtml, element) {
     var ids = {}
     var colorListsById = {}
     var boundsListsById = {}
     var originsById = {}
     var sizesById = {}
     var zoomsById = {}
+    var renderTargets = {}
 
     function htmlPainting(id) {
       if (ids[id]) {
@@ -26,7 +27,56 @@ module.exports = library.export(
       }
 
       colorListsById[id].push(color)
-      boundsListsById[id].push(color)
+      boundsListsById[id].push(bounds)
+
+      if (!renderTargets[id] || renderTargets[id].length < 1) { return }
+
+      renderStroke(renderTargets[id], color, bounds)
+    }
+
+    htmlPainting.playBackInto = function(id, selector) {
+      if (!id) {
+        throw new Error("No id")
+      }
+      htmlPainting.renderTo(id, selector)
+      var colors = colorListsById[id]
+      var bounds = boundsListsById[id]
+      var node = document.querySelector(selector)
+      for(var i=0; i<colors.length; i++) {
+        renderStroke(node, colors[i], bounds[i])
+      }
+    }
+
+    function renderStroke(target, color, bounds) {
+
+      var swatch = element(
+        element.style({
+          "position": "absolute",
+          "background": color,
+          "left": bounds.minX+"px",
+          "top": bounds.minY+"px",
+          "width": (bounds.maxX - bounds.minX)+"px",
+          "height": (bounds.maxY - bounds.minY)+"px",
+        })
+      )
+
+      if (Array.isArray(target)) {
+        target.forEach(addStroke)
+      } else {
+        addStroke(target)
+      }
+
+      function addStroke(node) {
+        addHtml.inside(node, swatch.html())
+      }
+    }
+
+    htmlPainting.renderTo = function(id, selector) {
+      var targets = renderTargets[id]
+      if (!targets) {
+        targets = renderTargets[id] = []
+      }
+      targets.push(document.querySelector(selector))
     }
 
     return htmlPainting
